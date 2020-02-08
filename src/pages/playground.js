9b020aa3d75c78759dd2be37dbaf161bc76c2922
hyperloo <hack.himanshu1024@gmail.com>
2020-02-09 00:09:12 +0530

    added related product

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Related from "../pages/Related";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
+			<Related />
 		</Layout>
 	);
 };
