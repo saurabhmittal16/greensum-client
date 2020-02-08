27239d08a44f42b9719fd53d38dbc17f6309a804
hiiambhanu <hiiambhanu@gmail.com>
2020-02-08 17:21:39 +0530

    Footer

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Footer from "../components/Footer";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
+			<Footer />
 		</Layout>
 	);
 };
