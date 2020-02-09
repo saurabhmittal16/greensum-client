a36676c1556146070c4ecd2c1ab38ca5e0a10998
Yatharth Vats <vatsyatharth2000@gmail.com>
2020-02-09 22:32:16 +0530

    Created a Review Form
    Currently testing review form on main page. Also added validation and a start rating value system.

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ReviewForm from "../components/ReviewForm";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
+
+			<ReviewForm />
 		</Layout>
 	);
 };
