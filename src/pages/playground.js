65b58e04323cb7ed99c52f64d13d6ea8bd643f1c
saurabhmittal16 <saurabhmittal1606@gmail.com>
2020-02-10 20:21:53 +0530

    Restructure Related component

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Related from "../components/Related";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
-			<Related />
 		</Layout>
 	);
 };
