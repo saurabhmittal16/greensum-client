c1227ff82d17845911763934628ea8770f3b5a9f
Hyperloo <hack.himanshu1024@gmail.com>
2020-02-10 15:56:40 +0530

    Added components from pages to components folder

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Related from "../pages/Related";
+import Related from "../components/Related";
 
 const Playground = () => {
 	return (
