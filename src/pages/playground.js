c16b6f12a87c196f5cd46dc329ce93e57ff2da44
elit-altum <manan.sharma311@gmail.com>
2020-04-08 20:47:47 +0530

    cleaned up code and added submit icon

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-
+import LoginPage from '../components/LoginPage';
 const Playground = () => {
 	return (
 		<Layout>
-			<h1>Test your components here</h1>
+			<LoginPage />
 		</Layout>
 	);
 };
