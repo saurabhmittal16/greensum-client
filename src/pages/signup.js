62dc4ae6ad4ae8470f4192c84ffc67a0e528d05b
saurabhmittal16 <saurabhmittal1606@gmail.com>
2020-04-09 23:14:43 +0530

    Add signup page

---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Signup from "../components/SignupPage";
+
+const Playground = () => {
+	return (
+		<Layout>
+			<Signup />
+		</Layout>
+	);
+};
+
+export default Playground;
