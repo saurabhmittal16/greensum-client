2eaaa020437ed2442cd4067eb66bd4d8816f1f2a
hiiambhanu <hiiambhanu@gmail.com>
2020-04-21 13:31:28 +0530

    changes

--- src/components/AllProducts/ListView/ProdListView.js
+++ src/components/AllProducts/ListView/ProdListView.js
@@ -1,16 +1,14 @@
 import React from "react";
 
 const Info = ({ name, price, text }) => (
-	<div 
+	<div
 		style={{
-			paddingRight:"5%",
-			
-	marginTop:"2em",
-	marginBottom:"2em",
+			paddingRight: "5%",
 		}}
 	>
-		<h2>{name}</h2>
-		<strong>&#8377; {price}/-</strong>
+		<h1>{name}</h1>
+		<h2><strong>&#8377; {price}/-</strong>
+		</h2>
 		<br />
 		<br />
 		<span>{text}</span>
@@ -22,11 +20,13 @@ export default function ProdListViewCard(props) {
 		<div style={parentDiv}>
 			<div style={flexContainer}>
 				<div style={content}>
-				<div
-				style={imageContainer}
-				>
-					<img src={props.image} alt="product image" style={image} />
-				</div>
+					<div style={imageContainer}>
+						<img
+							src={props.image}
+							alt="product image"
+							style={image}
+						/>
+					</div>
 					<Info
 						name={props.name}
 						price={props.price}
@@ -39,32 +39,29 @@ export default function ProdListViewCard(props) {
 }
 
 const parentDiv = {
-	marginLeft:"auto",
-	marginRight:"auto",
+	marginLeft: "auto",
+	marginRight: "auto",
 	marginTop: "2rem",
 	marginBottom: "2rem",
-	maxWidth:"1000px"
+	maxWidth: "1000px",
 };
 
 const flexContainer = {
 	display: "flex",
 	flexDirection: "row",
-
 };
 const imageContainer = {
-	minWidth:"11rem",
-	border:"green"
-	
+	minWidth: "12rem",
+	marginTop: "2em",
+	marginBottom: "2em",
+	marginRight:"2rem"
 };
 
 const image = {
 	marginTop: "auto",
 	marginBottom: "auto",
 	borderRadius: "1rem",
-	maxHeight:"150px",
-	width: "auto",
-	maxWidth:"100%",
-	height:"auto",
+	maxHeight: "300px",
 	transform: `translateX(${-50}%)`,
 };
 
