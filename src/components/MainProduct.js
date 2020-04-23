6c3ba4516339e56b48a58c3e4ea92f90e2d268fb
elit-altum <manan.sharma311@gmail.com>
2020-04-23 19:32:22 +0530

    add: added the mainProduct Image slider

--- src/components/MainProduct.js
+++ src/components/MainProduct.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import { Image, Rating, Icon, Form, Button } from "semantic-ui-react";
 
-// Handles product image
-const BorderedImage = ({ path }) => {
-	return (
-		<div
-			style={{
-				height: "100%",
-			}}
-		>
-			<Image src={path} bordered fluid />
-		</div>
-	);
-};
+const mockImages = [
+	"https://images.unsplash.com/photo-1558221693-89a565d7ecd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+	"https://images.unsplash.com/photo-1516100970402-530cfdf696d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+	"https://cdn0.weddingwire.com/emp/fotos/4/8/3/9/0/0/markus-spiske-gm0zvw3pzy-unsplash_51_9384-1569456136.jpg",
+	"https://images.unsplash.com/photo-1486297678162-eb2a19b0a32d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+];
 
 // Product rating and number or reviews
 const ProductRating = ({ rating = 3, max = 5, numberOfReviews }) => {
@@ -71,9 +65,7 @@ const ProductPrice = ({ price }) => {
 // Product description
 const ProductDescription = ({ description }) => {
 	return (
-		<div
-			className="product-header"
-		>
+		<div className="product-header">
 			<p>{description}</p>
 		</div>
 	);
@@ -186,7 +178,7 @@ const ProductCheckout = ({ maxItems = 7 }) => {
 								border: "none",
 								padding: "0.6rem",
 								marginLeft: "2rem",
-								boxShadow: "4px 4px 3px 0px #ccc",
+								boxShadow: "2px 3px 5px rgba(0, 0, 0, 0.2)",
 							}}
 						>
 							<Icon name="heart outline" size="large" />
@@ -215,7 +207,7 @@ const ProductUtilities = ({ category, tags }) => {
 					Tag:{" "}
 				</h2>
 				<div style={{ display: "flex" }}>
-					{tags.map(tag => (
+					{tags.map((tag) => (
 						<p key={tag} style={{ padding: "0.3rem" }}>
 							{tag}
 						</p>
@@ -271,42 +263,87 @@ const ProductUtilities = ({ category, tags }) => {
 	);
 };
 
+const ImageSlider = ({ images }) => {
+	let index = 0;
+
+	const [currentIndex, setCurrentIndex] = useState(0);
+	console.log(images[currentIndex]);
+
+	const slideLeft = () => {
+		if (!currentIndex) {
+			let newIndex = images.length - 1;
+			setCurrentIndex(newIndex);
+		} else {
+			let newIndex = currentIndex - 1;
+			setCurrentIndex(newIndex);
+		}
+	};
+
+	const slideRight = () => {
+		if (currentIndex == images.length - 1) {
+			let newIndex = 0;
+			setCurrentIndex(newIndex);
+		} else {
+			let newIndex = currentIndex + 1;
+			setCurrentIndex(newIndex);
+		}
+	};
+
+	const handleImageSelection = (index) => {
+		setCurrentIndex(index);
+	};
+
+	return (
+		<div className="main-product__slider">
+			<div className="slider-image-area">
+				<button className="scroll-button" onClick={slideLeft}>
+					<Icon name="angle left" />
+				</button>
+				<div className="slider-image">
+					<img src={images[currentIndex]} />
+				</div>
+				<button className="scroll-button" onClick={slideRight}>
+					<Icon name="angle right" />
+				</button>
+			</div>
+			<div className="slider-others">
+				{images.map((image, index) => (
+					<img
+						src={image}
+						onClick={() => handleImageSelection(index)}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
 // Final MainProduct component
 export default () => {
 	return (
-		<div className="container main">
+		<div
+			style={{
+				display: "flex",
+			}}
+			className="main-product"
+		>
+			<ImageSlider images={mockImages} />
 			<div
 				style={{
-					display: "flex",
+					flex: 1,
+					marginLeft: "2rem",
 				}}
 			>
-				<div
-					style={{
-						flex: 1,
-					}}
-				>
-					<BorderedImage path="/test-images/cheese-one.jpg" />
-				</div>
-				<div
-					style={{
-						flex: 1,
-						marginLeft: "2rem",
-					}}
-				>
-					<h1>Cheese Block</h1>
-					<ProductRating numberOfReviews={6} />
-					<ProductPrice price={150} />
-					<ProductDescription
-						description={
-							"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit autem at velit totam natus placeat consequatur. Et magni soluta nostrum voluptatibus voluptatem explicabo adipisci? Non, modi harum? Magni, eius repudiandae omnis similique dignissimos"
-						}
-					/>
-					<ProductCheckout />
-					<ProductUtilities
-						tags={["Food", "Cheese"]}
-						category="Food"
-					/>
-				</div>
+				<h1>Cheese Block</h1>
+				<ProductRating numberOfReviews={6} />
+				<ProductPrice price={150} />
+				<ProductDescription
+					description={
+						"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit autem at velit totam natus placeat consequatur. Et magni soluta nostrum voluptatibus voluptatem explicabo adipisci? Non, modi harum? Magni, eius repudiandae omnis similique dignissimos"
+					}
+				/>
+				<ProductCheckout />
+				<ProductUtilities tags={["Food", "Cheese"]} category="Food" />
 			</div>
 		</div>
 	);
