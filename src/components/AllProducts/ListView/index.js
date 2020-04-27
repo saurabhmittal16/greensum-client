3a8f7652d90e0d4028c06ab288f03b7c595670eb
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-04-27 17:30:41 +0530

    Restructure ProductListView

--- src/components/AllProducts/ListView/index.js
+++ src/components/AllProducts/ListView/index.js
@@ -1,7 +1,12 @@
 import ProdListViewCard from './ProdListView';
-const ProductListView = (props) => (
-    <ProdListViewCard 
-        image = {props.image}  name = {props.name}  price = {props.price}  text = {props.text}
-        />
-);
+
+const ProductListView = (props) => {
+    // data -> array of objects with data {image, name, price, text}
+    const data = props.data;
+    
+    return data.map(
+        item => <ProdListViewCard image = {item.image}  name = {item.name}  price = {item.price}  text = {item.text} />
+    );
+}
+
 export default ProductListView;
