f650b05618c14ccd1a9c52cd96c23b107677ff21
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-04-27 17:32:14 +0530

    Clear playground

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,19 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-import ProductListView from "../components/AllProducts/ListView";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
-			<ProductListView
-				data = {[
-					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
-					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
-					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
-					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
-				]}
-			/>
 		</Layout>
 	);
 };
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
-import MainProduct from "../components/MainProduct";
-import Review from "../components/Review";
-import Related from "../components/RelatedProducts";
+import ProductListView from "../components/AllProducts/ListView";
 
 const Shop = () => {
 	return (
 		<Layout>
-			<MainProduct />
-			<Review />
-			<Related />
+			<ProductListView
+				data = {[
+					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
+					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
+					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
+					{image: "/test-images/productImg.png", name: "Lorem Ipsum", price: "169", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum."},
+				]}
+			/>
 		</Layout>
 	);
 };
