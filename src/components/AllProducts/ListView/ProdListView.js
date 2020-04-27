f1b19a5b7d53d4c4e7e510162d4c271439a15b3f
Bhanu Pratap Singh <42886724+hiiambhanu@users.noreply.github.com>
2020-04-27 17:21:47 +0530

    Product list view (#24)
    * Add list view for AllProducts
    * product list view redo
    * removed negative margins
    * ListView
    * removed inline styles
    
    Co-authored-by: Saurabh Mittal <saurabhmittal1606@gmail.com>

---
+++ src/components/AllProducts/ListView/ProdListView.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Info = ({ name, price, text }) => (
+	<div className="info">
+		<h1>{name}</h1>
+		<h2>
+			<strong>&#8377; {price}/-</strong>
+		</h2>
+		<br />
+		<br />
+		<span>{text}</span>
+	</div>
+);
+
+export default function ProdListViewCard(props) {
+	return (
+		<div className="parentDiv">
+			<div className="flexContainer">
+				<div className="imageContainer">
+					<img
+						className="image"
+						src={props.image}
+						alt="product image"
+					/>
+				</div>
+				<div className="content">
+					<Info
+						name={props.name}
+						price={props.price}
+						text={props.text}
+					></Info>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout";
-import SignupPage from "../components/LoginPage";
+import ProductListView from "../components/AllProducts/ListView";
 
 const Playground = () => {
 	return (
 		<Layout>
-			<SignupPage />
+			<h1>Test your components here</h1>
+			<ProductListView image = "/test-images/productImg.png" name = " Lorem Ipsum " price = "169" text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum.  "/>
+			<ProductListView image = "/test-images/avatar.png" name = " Lorem Ipsum " price = "169" text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum.  "/>
+			<ProductListView image = "/test-images/Milk-carton-mockup.jpg" name = " Lorem Ipsum " price = "169" text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum.  "/>
+			<ProductListView image = "/test-images/cheese-one.jpg" name = " Lorem Ipsum " price = "169" text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet erat nec lorem auctor fermentum nec sit amet ipsum. Etiam tempus id mauris quis interdum. Ut vestibulum posuere bibendum.  "/>
 		</Layout>
 	);
 };
