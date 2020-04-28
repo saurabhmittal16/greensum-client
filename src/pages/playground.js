b9017bda5e75cd5e04df6e851170324af5e72ca0
hiiambhanu <hiiambhanu@gmail.com>
2020-04-28 14:00:26 +0530

    banner

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Banner from "../components/LandingPage/Banner";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
+			<Banner/>
 		</Layout>
 	);
 };
