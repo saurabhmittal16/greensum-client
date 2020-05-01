7e44ff25688ed749de14881909219bfe899e1ab9
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-05-02 00:37:13 +0530

    Add tentative carousel

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Carousel from "../components/RelatedProducts/Carousel";
 
 const Playground = () => {
 	return (
 		<Layout>
 			<h1>Test your components here</h1>
+			<Carousel />
 		</Layout>
 	);
 };
