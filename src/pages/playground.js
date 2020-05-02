655eeaec9443faaa382deddc22c8ecab47d52d16
hyperloo <hack.himanshu1024@gmail.com>
2020-05-02 22:18:16 +0530

    temp canges

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,14 +1,9 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Hottest from "../components/HottestDeal";
+import MyAccount from "../components/MyAccount";
 
 const Playground = () => {
-	return (
-		<Layout>
-			{/* <h1>Test your components here</h1> */}
-			<Hottest />
-		</Layout>
-	);
+	return <Layout>{/* <h1>Test your components here</h1> */}</Layout>;
 };
 
 export default Playground;
