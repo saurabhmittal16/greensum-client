8a8087cd5db47566425391e95c7c80fab0cbacae
elit-altum <manan.sharma311@gmail.com>
2020-05-02 20:21:05 +0530

    remove: removed playgroud test file

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Hottest from "../components/RelatedProducts/Carousel";
 
 const Playground = () => {
 	return (
 		<Layout>
-			{/* <h1>Test your components here</h1> */}
-			<Hottest />
+			<h1>Test your components here</h1>
 		</Layout>
 	);
 };
