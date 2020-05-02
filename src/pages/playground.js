49f206ca1a1e2da5bf2431a3d5d15d151a866ef3
hyperloo <hack.himanshu1024@gmail.com>
2020-05-02 22:25:08 +0530

    My account section added

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../components/Layout";
-import MyAccount from "../components/MyAccount";
 
 const Playground = () => {
 	return <Layout>{/* <h1>Test your components here</h1> */}</Layout>;
