401ce67dc7f36c13910c5b21855e17f08a82d5c0
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-05-16 19:09:33 +0530

    Add checkout page

---
+++ src/pages/checkout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Checkout from "../components/Checkout";
+
+const Contact = () => {
+	return (
+		<Layout heading="Checkout">
+			<Checkout />
+		</Layout>
+	);
+};
+
+export default Contact;
