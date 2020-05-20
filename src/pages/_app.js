d846785213e381e56aceeba04b29c3a086397b13
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-05-20 18:45:06 +0530

    Remove redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,30 +1,7 @@
-import { Provider } from "react-redux";
-import App from "next/app";
-import withRedux from "next-redux-wrapper";
 import "semantic-ui-css/semantic.min.css";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import "../styles/main.scss";
-import { initStore } from "../redux";
 
-class MyApp extends App {
-	static async getInitialProps({ Component, ctx }) {
-		return {
-			pageProps: {
-				...(Component.getInitialProps
-					? await Component.getInitialProps(ctx)
-					: {}),
-			},
-		};
-	}
-
-	render() {
-		const { Component, pageProps, store } = this.props;
-		return (
-			<Provider store={store}>
-				<Component {...pageProps} />
-			</Provider>
-		);
-	}
+export default function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
 }
-
-export default withRedux(initStore, { debug: true })(MyApp);
--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import tempReducer from "./temp";
-
-export default combineReducers({
-	temp: tempReducer,
-});
